63809f67cef5633f6091eebcc71a1cab5e796d11
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-01-13 09:29:14 -0700

    change: use slice::as_simd()

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,32 @@ pub mod prelude {
     pub use crate::problem_1;
 }
 
+/// Solves
 pub fn problem_1(array: &[i16]) -> i16 {
     let zero = i16x8::splat(0);
 
     let mut positives = i16x8::splat(0);
     let mut negatives = i16x8::splat(0);
 
-    let mut i = 0;
-    while i + 7 < array.len() {
-        let a = i16x8::from_slice(&array[i..i + 8]);
-        positives += a.simd_gt(zero).to_int();
-        negatives += a.simd_lt(zero).to_int();
-        i += 8;
+    let (prefix, middle, suffix) = array.as_simd();
+
+    for chunk in middle {
+        positives += chunk.simd_gt(zero).to_int();
+        negatives += chunk.simd_lt(zero).to_int();
     }
 
-    let n_positive = -positives.reduce_sum();
-    let n_negative = -negatives.reduce_sum();
+    let mut n_positive = -positives.reduce_sum();
+    let mut n_negative = -negatives.reduce_sum();
+
+    for n in prefix.iter().copied() {
+        n_positive += (n > 0) as i16;
+        n_negative += (n < 0) as i16;
+    }
+
+    for n in suffix.iter().copied() {
+        n_positive += (n > 0) as i16;
+        n_negative += (n < 0) as i16;
+    }
 
     std::cmp::max(n_positive, n_negative)
 }
