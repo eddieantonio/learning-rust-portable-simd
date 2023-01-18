12e6ee46da7a0e81dff44cf2c896e7ab5d89904c
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-01-18 21:52:26 +0000

    bench: add bench support

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 #![feature(portable_simd)]
+#![feature(test)]
+
+#[cfg(test)]
+extern crate test;
 
 pub mod data;
 pub mod implementations;
@@ -12,6 +16,7 @@ pub mod prelude {
 
 #[cfg(test)]
 mod tests {
+    use test::Bencher;
 
     macro_rules! impl_test {
         ($mod: ident) => {
@@ -26,4 +31,24 @@ mod tests {
     impl_test!(simd);
     impl_test!(fold);
     impl_test!(c);
+
+    use crate::data;
+
+    #[bench]
+    fn bench_c(b: &mut Bencher) {
+        use crate::implementations::c::problem_1;
+        b.iter(|| problem_1(&data::ARRAY));
+    }
+
+    #[bench]
+    fn bench_fold(b: &mut Bencher) {
+        use crate::implementations::fold::problem_1;
+        b.iter(|| problem_1(&data::ARRAY));
+    }
+
+    #[bench]
+    fn bench_simd(b: &mut Bencher) {
+        use crate::implementations::simd::problem_1;
+        b.iter(|| problem_1(&data::ARRAY));
+    }
 }
