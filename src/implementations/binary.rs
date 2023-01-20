52738db265e82184e005f53dfa8994f2b1c6ba93
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-01-20 22:21:52 +0000

    refactor(test): use test_case crate

--- src/implementations/binary.rs
+++ src/implementations/binary.rs
@@ -35,81 +35,21 @@ fn find_lower_range_start(nums: &[i16]) -> Option<usize> {
     Some(high)
 }
 
-fn find_lower_range_start(nums: &[i16]) -> Option<usize> {
-    let mut low = 0;
-    let mut high = nums.len();
-
-    if nums.is_empty() {
-        return None;
-    }
-
-    if nums[0] > 0 {
-        // The first element is greater than zero.  If it's in non-decreasing order, that means it
-        // can't contain zero!
-        return None;
-    }
-
-    if nums[0] == 0 {
-        // Avoid looping with this one weird trick!
-        return Some(0);
-    }
-
-    while high - low > 1 {
-        let midpoint = low + (high - low) / 2;
-        if nums[midpoint] >= 0 {
-            // Eliminate the right-hand side to search in the left.
-            high = midpoint;
-        } else {
-            // Eliminate the left-hand side to search in the right.
-            low = midpoint;
-        }
-    }
-
-    Some(high)
-}
-
-
 #[cfg(test)]
 mod test {
     use super::*;
+    use test_case::test_case;
 
-    #[test]
-    fn test_find_lower_range() {
-        assert_eq!(Some(1), find_lower_range_start(&[-1, 0, 1]));
-    }
-
-    #[test]
-    fn test_find_lower_range_2() {
-        assert_eq!(Some(3), find_lower_range_start(&[-1, -1, -1]));
-    }
-
-    #[test]
-    fn test_find_lower_range_3() {
-        assert_eq!(None, find_lower_range_start(&[1, 1, 1]));
-    }
-
-    #[test]
-    fn test_find_lower_range_4() {
-        assert_eq!(Some(0), find_lower_range_start(&[0]));
-    }
-
-    #[test]
-    fn test_find_lower_range_5() {
-        assert_eq!(Some(3), find_lower_range_start(&[-1, -1, -1, 0]));
-    }
-
-    #[test]
-    fn test_find_lower_range_6() {
-        assert_eq!(Some(1), find_lower_range_start(&[-1, 0, 1, 1]));
-    }
-
-    #[test]
-    fn test_find_lower_range_7() {
-        assert_eq!(Some(1), find_lower_range_start(&[-1, 0, 0, 1]));
-    }
-
-    #[test]
-    fn test_find_lower_range_8() {
-        assert_eq!(Some(2), find_lower_range_start(&[-1, -1, 0, 1]));
+    #[test_case(Some(1), &[-1, 0, 1] ; "Basic")]
+    #[test_case(Some(3), &[-1, -1, -1] ; "All negative 1")]
+    #[test_case(None, &[1, 1, 1] ; "All 1")]
+    #[test_case(Some(0), &[0] ; "Just zero")]
+    #[test_case(Some(3), &[-1, -1, -1, 0] ; "Zero at end")]
+    #[test_case(Some(0), &[0, 1, 1, 1, 1] ; "Zero at beginning")]
+    #[test_case(Some(1), &[-1, 0, 1, 1] ; "Zero off from middle")]
+    #[test_case(Some(1), &[-1, 0, 0, 1] ; "Span of zeros")]
+    #[test_case(Some(2), &[-1, -1, 0, 1] ; "Zero off from middle 1")]
+    fn test_find_lower_range(expected: Option<usize>, array: &[i16]) {
+        assert_eq!(expected, find_lower_range_start(array));
     }
 }
