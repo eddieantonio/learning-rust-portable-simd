aeb1a984918de9c869746ce73eac8238958b499d
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2023-01-20 22:54:26 +0000

    [wip]: my brain hurts

--- src/implementations/binary.rs
+++ src/implementations/binary.rs
@@ -35,6 +35,40 @@ fn find_lower_range_start(nums: &[i16]) -> Option<usize> {
     Some(high)
 }
 
+// Finds the first number greater than zero
+fn find_upper_range_start(nums: &[i16]) -> Option<usize> {
+    let mut low = 0;
+    let mut high = nums.len();
+
+    if nums.is_empty() {
+        return None;
+    }
+
+    if nums[0] > 0 {
+        // The first element is greater than zero.  If it's in non-decreasing order, that means it
+        // can't contain zero!
+        return None;
+    }
+
+    if nums[nums.len() - 1] == 0 {
+        // Avoid looping with this one weird trick!
+        return Some(nums.len());
+    }
+
+    while high > low {
+        let midpoint = low + (high - low) / 2;
+        if nums[midpoint] <= 0 {
+            // Eliminate the left-hand side to search on the right.
+            low = midpoint + 1;
+        } else {
+            // Eliminate the right-hand side to search on the left.
+            high = midpoint;
+        }
+    }
+
+    Some(low)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -51,5 +85,18 @@ mod test {
     #[test_case(Some(2), &[-1, -1, 0, 1] ; "Zero off from middle 1")]
     fn test_find_lower_range(expected: Option<usize>, array: &[i16]) {
         assert_eq!(expected, find_lower_range_start(array));
+    }
+
+    #[test_case(Some(2), &[-1, 0, 1] ; "Basic")]
+    #[test_case(Some(3), &[-1, -1, -1] ; "All negative 1")]
+    #[test_case(None, &[1, 1, 1] ; "All 1")]
+    #[test_case(Some(1), &[0] ; "Just zero")]
+    #[test_case(Some(4), &[-1, -1, -1, 0] ; "Zero at end")]
+    #[test_case(Some(1), &[0, 1, 1, 1, 1] ; "Zero at beginning")]
+    #[test_case(Some(2), &[-1, 0, 1, 1] ; "Zero off from middle")]
+    #[test_case(Some(3), &[-1, 0, 0, 1] ; "Span of zeros")]
+    #[test_case(Some(3), &[-1, -1, 0, 1] ; "Zero off from middle 1")]
+    fn test_find_upper_range(expected: Option<usize>, array: &[i16]) {
+        assert_eq!(expected, find_upper_range_start(array));
     }
 }
